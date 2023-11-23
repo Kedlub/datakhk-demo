ea0cc888c80ec00af78b81000d3977ee5d9e8c3b
Kedlub <jakubkyzr@gmail.com>
2023-11-23 18:46:24 +0100

    Better comment

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -11,7 +11,9 @@ interface School {
   };
 }
 
-const DATASET_URL = 'https://services6.arcgis.com/ogJAiK65nXL1mXAW/arcgis/rest/services/Seznam_škol_a_školských_zařízení/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson'; // API URL získaná z informací o datasetu
+// API URL získaná z informací o datasetu
+// https://www.datakhk.cz/datasets/fc365d95ea124c6fb18540f5e5a2eef1_0/api
+const DATASET_URL = 'https://services6.arcgis.com/ogJAiK65nXL1mXAW/arcgis/rest/services/Seznam_škol_a_školských_zařízení/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson';
 
 export const getSchools = async () => {
   const response = await fetch(DATASET_URL);
